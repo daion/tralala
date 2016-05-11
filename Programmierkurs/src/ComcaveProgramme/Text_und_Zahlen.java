d496d72416fe299d05c2d07ce87cecaf9594fe2f
stoellner <sventoellner@gmx.net>
2016-05-11 11:50:02 +0200

    update

---
+++ Programmierkurs/src/ComcaveProgramme/Text_und_Zahlen.java
@@ -0,0 +1,28 @@
+package ComcaveProgramme;
+
+public class Text_und_Zahlen { // Anfang
+
+// hier kommt der Code
+
+  // hier brauchen wir die main Methode
+	
+	public static void main(String[] args) { //main Anfang
+		
+		
+		//Datentypen
+		int zahl1 =3;
+		int zahl2 =2;
+		int erg = zahl1+zahl2;
+		int ergp = zahl1*zahl2;
+		int ergd = zahl1/zahl2;
+		
+		
+		System.out.println("Gegeben sind zwei Zahlen: " + zahl1 + " und " + zahl2);
+		System.out.println("Die Summe der Zahlen " + zahl1 + " und " + zahl2 + " ist " + erg);
+		System.out.println("Das Produkt der Zahlen " + zahl1 + " und " + zahl2 + " ist " + ergp );
+		System.out.println("Die Differenz der Zahlen " + zahl1 + " und " + zahl2 + " ist " + ergd );
+		
+	}//main Ende
+	
+	
+} //Ende
